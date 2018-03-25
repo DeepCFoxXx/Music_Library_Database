02ebdffd17f2e937337ae6c89773c3b9bd988d88
DeepCFoxXx <learncode23@gmail.com>
2018-03-25 18:58:06 +0100

    2nd table added

--- db/music_library.sql
+++ db/music_library.sql
@@ -1,6 +1,17 @@
+DROP TABLE albums;
 DROP TABLE artists;
 
 CREATE TABLE artists (
   id SERIAL4 PRIMARY KEY,
   name VARCHAR(255)
 );
+
+CREATE TABLE albums (
+  id SERIAL4 PRIMARY KEY ,
+  name VARCHAR(255),
+  artist_id int4 REFERENCES artists(id)
+);
+
+--  createdb bounty_hunter
+--  psql -d bounty_hunter -f db/bounty_hunter.sql
+--  ruby db/console.rb
