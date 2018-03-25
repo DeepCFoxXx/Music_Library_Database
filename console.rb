dc499f422f4bec2be524533908f18976ac21d37e
DeepCFoxXx <learncode23@gmail.com>
2018-03-25 19:37:03 +0100

    seeds added

---
+++ console.rb
@@ -0,0 +1,11 @@
+require_relative( './models/album' )
+require_relative( './models/artist' )
+
+artist1 = Artist.new( { 'name' => 'Queens Of The Stone Age' } )
+artist1.save();
+
+album1 = Album.new( { 'name' => 'Songs For The Deaf', 'artist_id' => artist1.id } )
+album1.save()
+
+album2 = Album.new( { 'name' => 'Rated R', 'artist_id' => artist1.id } )
+album2.save()
