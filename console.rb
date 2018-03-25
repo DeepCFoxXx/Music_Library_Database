ff9286e0f3513cc9108d43e685cc62e8a6b0bc76
DeepCFoxXx <learncode23@gmail.com>
2018-03-25 19:39:53 +0100

    added pry

--- console.rb
+++ console.rb
@@ -1,5 +1,7 @@
 require_relative( './models/album' )
 require_relative( './models/artist' )
+require( 'pry-byebug' )
+
 
 artist1 = Artist.new( { 'name' => 'Queens Of The Stone Age' } )
 artist1.save();
@@ -9,3 +11,6 @@ album1.save()
 
 album2 = Album.new( { 'name' => 'Rated R', 'artist_id' => artist1.id } )
 album2.save()
+
+binding.pry
+nil
