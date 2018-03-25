ac4b11324a25372dc5f56cef070dcaca45d9a8f7
DeepCFoxXx <learncode23@gmail.com>
2018-03-25 19:26:52 +0100

    save added

---
+++ models/artist.rb
@@ -0,0 +1,26 @@
+require_relative( '../db/sql_runner' )
+
+class Artist
+
+  attr_reader( :id, :name )
+
+  def initialize( options )
+    @id = options['id'].to_i
+    @name = options['name']
+  end
+
+  def save()
+    sql = "INSERT INTO artists
+    (
+      name
+    )
+    VALUES
+    (
+    $1
+    )
+    RETURNING *"
+    values = [@name]
+    @id = SqlRunner.run( sql, values )[0]["id"].to_i()
+  end
+
+end   
